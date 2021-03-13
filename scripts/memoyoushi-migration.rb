a8b8d1e546b04d04d33e4b7d48a950661fa6393f
Osamu Koga <osak.63@gmail.com>
2021-03-12 22:00:26 -0600

    Add google analytics to old pages

--- scripts/memoyoushi-migration.rb
+++ scripts/memoyoushi-migration.rb
@@ -19,19 +19,47 @@ def sed_i(filename, &block)
     end
 end
 
+GOOGLE_ANALYTICS = <<END
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-K7GK6FJ729"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-K7GK6FJ729');
+</script>
+END
+GOOGLE_ANALYTICS_MARK = GOOGLE_ANALYTICS.lines.first
+
+def patch_html(filename)
+    has_analytics = false
+    sed_i(filename) { |line|
+        if line == GOOGLE_ANALYTICS_MARK
+            has_analytics = true
+        end
+        if line.match(/<script src=/)
+            ""
+        else
+            result = line.gsub(/(<link.*)style.css(")/) { "#{$1}style_memoyoushi.css#{$2}" }
+                .gsub(%r{(<a.*href=".*)index.html(".*>メモ用紙2.0</a>)}) { "#{$1}index_memoyoushi.html#{$2}" }
+                .gsub(%r{(class="kogarasi-mark".*>)(</div>)}) { "#{$1}ここにはかつてコメントが表示されていました#{$2}" }
+            if !has_analytics && line.match(%r{</head>})
+                result = "#{GOOGLE_ANALYTICS}\n</head>"
+            end
+            result
+        end
+    }
+end
+
 dirs = %w(diary trip program other hardware linux food)
 
 dirs.each do |dir|
     Dir.glob("memoyoushi-archive/#{dir}/**/*.html") do |path|
         puts path
-        sed_i(path) { |line|
-            if line.match(/<script src=/)
-                ""
-            else
-                line.gsub(/(<link.*)style.css(")/) { "#{$1}style_memoyoushi.css#{$2}" }
-                    .gsub(%r{(<a.*href=".*)index.html(".*>メモ用紙2.0</a>)}) { "#{$1}index_memoyoushi.html#{$2}" }
-                    .gsub(%r{(class="kogarasi-mark".*>)(</div>)}) { "#{$1}ここにはかつてコメントが表示されていました#{$2}" }
-            end
-        }
+        patch_html(path)
     end
 end
+patch_html("memoyoushi-archive/about_me.html")
+patch_html("memoyoushi-archive/about.html")
+patch_html("memoyoushi-archive/index_memoyoushi.html")
